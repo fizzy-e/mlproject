5cd13a5439a33b753f29f07ddd991048e7e0c255
M Faizan <muhdfaizan0312@gmail.com>
2025-09-09 06:19:18 +0500

    Data Transformation done

--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,23 @@
 # any functionality that will be used in a common way, in the entire project
 # like creating a mongo db client
+
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+import dill
+
+from src.exception import CustomException
+
+def save_object(file_path,obj):
+    try:
+        dir_path=os.path.dirname(file_path)
+
+        os.makedirs(dir_path,exist_ok=True)
+
+        with open(file_path,'wb') as file_obj:
+            dill.dump(obj,file_obj)
+
+    except Exception as e:
+        raise CustomException(e,sys)
